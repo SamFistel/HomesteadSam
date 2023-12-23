95b9797c9cedbe7e4faadff5f2b3300c873453e0
Peter <chuy1483@gmail.com>
2023-12-23 05:22:42 -0500

    Added URI for 2024 and favicon

--- src/BibInput.js
+++ src/BibInput.js
@@ -10,8 +10,9 @@ const BibInput = ({ onObjectReceived }) => {
     
     const handleButtonClick = () => {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://my4.raceresult.com/192607/RRPublish/data/list?key=9d484a9a9259ff0ae1a4a8570861bc3b&fav=&listname=Online%7CLap%20Details&page=live&contest=0&r=bib2&bib=' + inputValue); // 2022
+        xhr.open('GET', 'https://my4.raceresult.com/192607/RRPublish/data/list?key=9d484a9a9259ff0ae1a4a8570861bc3b&listname=Online%7CLap%20Details&page=live&contest=0&r=bib2&bib=' + inputValue); // 2022
         // xhr.open('GET', 'https://my4.raceresult.com/204047/RRPublish/data/list?key=b02d8bcb6d81d09372a43de65f7f7d48&listname=Online%7CLap%20Details&page=live&contest=0&r=bib2&bib=' + inputValue); // 2023
+        // xhr.open('GET', 'https://my1.raceresult.com/259072/RRPublish/data/list?key=eca2e3d1510caee33b7710a250a6f2c1&listname=Online%7CLap%20Details&page=live&contest=0&r=bib2&bib=' + inputValue); // 2024
         xhr.onload = function () {
           if (xhr.status === 200) {
             onObjectReceived(JSON.parse(xhr.responseText));
